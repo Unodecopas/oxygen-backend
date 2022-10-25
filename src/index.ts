b2c4d588636aad8650c92edbb9498dbd77b32a5a
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-10-26 01:38:59 +0200

    fix: jwt expires days

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import app from './app'
 import 'dotenv/config'
 
-const PORT = !isNaN(process.env.PORT) ? process.env.PORT : 4000
+const PORT = (process.env.PORT !== undefined) ? process.env.PORT : 4000
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
